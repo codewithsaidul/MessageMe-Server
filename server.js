c90694309279706a3d557968aaf7f0c5d8f4e3a9
codewithsaidul <codewithsaidul@gmail.com>
2024-08-24 11:58:14 +0600

    Server Setup & Database Setup complited

---
+++ server.js
@@ -0,0 +1,56 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+dotenv.config({path: "./config.env"})
+
+process.on("uncaughtException", (err) => {
+    console.log(err);
+    process.exit(1)
+})
+
+
+
+
+const http = require("http");
+
+const server =  http.createServer(app);
+
+
+const DB = process.env.DBURI.replace("<db_password>", process.env.DBPASSWORD);
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedToplogy: true,
+}.then((con) => {
+    console.log("DB Connection Successfull")
+})
+.catch((err) => {
+    console.log(err)
+})
+)
+
+const port = process.env.PORT || 5000;
+
+
+
+
+
+
+
+
+server.listen(port, () => {
+    console.log(`App is Running on ${port}`)
+});
+
+
+
+
+
+
+process.on("unhandledRejection", (err) => {
+    console.log(err);
+    server.close(() => {
+        process.exit(1)
+    })
+})
